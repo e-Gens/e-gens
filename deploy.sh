1340ac784c365a4e97a915395d995f27da0b886e
Elias Alves <unixelias@gmail.com>
2018-10-24 23:13:44 -0300

    Fix deploy 9
    Nomes de variáveis

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 # Variáveis via Travis Secrets
 
-sshpass -p $PASSWD sftp -P 2222 -v -o StrictHostKeyChecking=no $USER@$HOST <<END_SCRIPT
+sshpass -p $FTP_PASSWD sftp -P 2222 -v -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST <<END_SCRIPT
 progress
 rm $REMOTE_ROOT/$LOCAL_ROOT/$FILE
+rm $REMOTE_ROOT/$LOCAL_ROOT/css/$FILE
+rm $REMOTE_ROOT/$LOCAL_ROOT/js/$FILE
+rm $REMOTE_ROOT/$LOCAL_ROOT/img/$FILE
+progress
 put -r $LOCAL_ROOT $REMOTE_ROOT
 bye
 END_SCRIPT
