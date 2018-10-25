b37a84ac1bef541bf0d67cad44805a6a548036d1
Elias Alves <unixelias@gmail.com>
2018-10-25 00:07:56 -0300

    Fix deploy final

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 # Variáveis via Travis Secrets
 
-sshpass -p $FTP_PASSWD sftp -P 2222 -v -o IdentityFile='src/assets/ssh/id_rsa' $FTP_USER@$FTP_HOST <<END_SCRIPT
+# ==================================================
+# sshpass => programa para entrada de senha no sftp
+# -p [] => senha
+# --------------------------------------------------
+# sftp => conexão via ssh para upload de arquivos
+# -P [] => porta da conexão 
+# -v => (verbose) com log de execução
+# -o => opção ssh - sem validação restrita por chaves
+# --------------------------------------------------
+sshpass -p $FTP_PASSWD sftp -P $FTP_PORT -v -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST <<END_SCRIPT
 progress
 rm $REMOTE_ROOT/$LOCAL_ROOT/$FILE
 rm $REMOTE_ROOT/$LOCAL_ROOT/css/$FILE
@@ -12,4 +21,10 @@ put -r $LOCAL_ROOT $REMOTE_ROOT
 bye
 END_SCRIPT
 exit 0
+# ==================================================
+# rm => remove arquivos no servidor
+# put => envia arquivos ao servidor
+# progress => exibe o progresso
+# bye => encerra a conexão
+# --------------------------------------------------
 
