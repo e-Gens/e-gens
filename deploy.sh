8b28a790829dbc8f16851a366b68aeb236dd278d
Elias Alves <unixelias@gmail.com>
2018-10-29 11:36:49 -0300

    Refatora script deploy
    Deploy apenas no master e não PR

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 # Variáveis via Travis Secrets
 
+
+# ==================================================
+# rm => remove arquivos no servidor
+# put => envia arquivos ao servidor
+# progress => exibe o progresso
+# bye => encerra a conexão
+# --------------------------------------------------
+SCRIPT="progress;
+rm $REMOTE_ROOT/$LOCAL_ROOT/$FILE;
+rm $REMOTE_ROOT/$LOCAL_ROOT/css/$FILE;
+rm $REMOTE_ROOT/$LOCAL_ROOT/js/$FILE;
+rm $REMOTE_ROOT/$LOCAL_ROOT/img/$FILE;
+progress;
+put -r $LOCAL_ROOT $REMOTE_ROOT;
+bye;
+"
+
 # ==================================================
 # sshpass => programa para entrada de senha no sftp
 # -p [] => senha
@@ -10,21 +27,11 @@
 # -v => (verbose) com log de execução
 # -o => opção ssh - sem validação restrita por chaves
 # --------------------------------------------------
-sshpass -p $FTP_PASSWD sftp -P $FTP_PORT -v -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST <<END_SCRIPT
-progress
-rm $REMOTE_ROOT/$LOCAL_ROOT/$FILE
-rm $REMOTE_ROOT/$LOCAL_ROOT/css/$FILE
-rm $REMOTE_ROOT/$LOCAL_ROOT/js/$FILE
-rm $REMOTE_ROOT/$LOCAL_ROOT/img/$FILE
-progress
-put -r $LOCAL_ROOT $REMOTE_ROOT
-bye
-END_SCRIPT
+if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]
+# Deploy apenas no [master]
+then
+    sshpass -p $FTP_PASSWD sftp -P $FTP_PORT -v -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST $SCRIPT
+fi
 exit 0
-# ==================================================
-# rm => remove arquivos no servidor
-# put => envia arquivos ao servidor
-# progress => exibe o progresso
-# bye => encerra a conexão
-# --------------------------------------------------
+
 
